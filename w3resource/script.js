c69961c7c3c2fac1d5955f549d49fb3c787e004c
miomaeshima <miomamomo@gmail.com>
2021-09-05 18:40:08 +0900

    w3resource no.3

--- w3resource/script.js
+++ w3resource/script.js
@@ -3,7 +3,7 @@
 // JavaScript Recursion [9 exercises with solution]
 // Use recursion to solve the following exercises.
 
-// 1. Write a JavaScript program to calculate the factorial of a number. Go to the editor
+// 1. Write a JavaScript program to calculate the factorial of a number.
 // In mathematics, the factorial（階乗） of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example, 5! = 5 x 4 x 3 x 2 x 1 = 120
 //factorial(0) and factorial(1) should be both 1
 
@@ -37,34 +37,39 @@ function find() {
   return 3;
 }
 
-// 3. Write a JavaScript program to get the integers in range (x, y). Go to the editor
+// 3. Write a JavaScript program to get the integers in range (x, y).
 // Example : range(2, 9)
 // Expected Output : [3, 4, 5, 6, 7, 8]
-// Click me to see the solution.
 
-// 4. Write a JavaScript program to compute the sum of an array of integers. Go to the editor
+function range(x, y, result = []) {
+  let min = Math.min(x, y);
+  let max = Math.max(x, y);
+  if (min === max) {
+    result.push(min);
+    return result;
+  }
+  result.push(min);
+  min++;
+  return range(min, max, result);
+}
+
+// 4. Write a JavaScript program to compute the sum of an array of integers.
 // Example : var array = [1, 2, 3, 4, 5, 6]
 // Expected Output : 21
-// Click me to see the solution.
 
-// 5. Write a JavaScript program to compute the exponent of a number. Go to the editor
+// 5. Write a JavaScript program to compute the exponent of a number.
 // Note : The exponent of a number says how many times the base number is used as a factor.
 // 82 = 8 x 8 = 64. Here 8 is the base and 2 is the exponent.
-// Click me to see the solution.
 
-// 6. Write a JavaScript program to get the first n Fibonacci numbers. Go to the editor
+// 6. Write a JavaScript program to get the first n Fibonacci numbers.
 // Note : The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . Each subsequent number is the sum of the previous two.
-// Click me to see the solution.
 
-// 7. Write a JavaScript program to check whether a number is even or not. Go to the editor
-// Click me to see the solution.
+// 7. Write a JavaScript program to check whether a number is even or not.
 
-// 8. Write a JavaScript program for binary search. Go to the editor
+// 8. Write a JavaScript program for binary search.
 // Sample array : [0,1,2,3,4,5,6]
 // console.log(l.br_search(5)) will return '5'
-// Click me to see the solution.
 
-// 9. Write a merge sort program in JavaScript. Go to the editor
+// 9. Write a merge sort program in JavaScript.
 // Sample array : [34,7,23,32,5,62]
 // Sample output : [5, 7, 23, 32, 34, 62]
-// Click me to see the solution.
